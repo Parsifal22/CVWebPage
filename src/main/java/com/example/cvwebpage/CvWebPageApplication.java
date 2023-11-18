8796415f8e4bb5888347ac2ec88c44768033f602
Parsifal22 <73550345+Parsifal22@users.noreply.github.com>
2023-11-18 20:52:48 +0200

    First commit

---
+++ src/main/java/com/example/cvwebpage/CvWebPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.cvwebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvWebPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvWebPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cvwebpage/CvWebPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cvwebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvWebPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
